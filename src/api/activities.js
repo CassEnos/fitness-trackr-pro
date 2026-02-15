f1cae4a647bf456f34277128f8bfd2928c86e5bd
Cass Enos <enosdoc@gmail.com>
2026-02-15 13:21:14 -0800

    final commit

--- src/api/activities.js
+++ src/api/activities.js
@@ -1,6 +1,5 @@
 const API = import.meta.env.VITE_API;
 
-/** Fetches an array of activities from the API. */
 export async function getActivities() {
   try {
     const response = await fetch(API + "/activities");
@@ -12,10 +11,17 @@ export async function getActivities() {
   }
 }
 
-/**
- * Sends a new activity to the API to be created.
- * A valid token is required.
- */
+export async function getActivity(id) {
+  try {
+    const response = await fetch(API + "/activities/" + id);
+    const result = await response.json();
+    return result;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
 export async function createActivity(token, activity) {
   if (!token) {
     throw Error("You must be signed in to create an activity.");
@@ -36,10 +42,6 @@ export async function createActivity(token, activity) {
   }
 }
 
-/**
- * Requests the API to delete the activity with the given ID.
- * A valid token is required.
- */
 export async function deleteActivity(token, id) {
   if (!token) {
     throw Error("You must be signed in to delete an activity.");
